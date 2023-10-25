915b983650eddb67ec574cd6e6743d60e6325b0f
Saeed Molaee <saeedmolaiy.dev@gmail.com>
2023-10-25 14:51:43 +0330

    Selection Sort

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] arr = { 64, 25, 12, 22, 11 };
+
+Console.WriteLine("Original array:");
+PrintArray(arr);
+
+SelectionSort.SelectionSort.SelectionSortAlgorithm(arr);
+
+Console.WriteLine("\nSorted array:");
+PrintArray(arr);
+
+static void PrintArray(IEnumerable<int> arr)
+{
+    foreach (var num in arr)
+    {
+        Console.Write(num + " ");
+    }
+
+    Console.WriteLine();
+}
---
+++ SelectionSort/SelectionSort.cs
@@ -0,0 +1,26 @@
+﻿namespace SelectionSort;
+
+internal static class SelectionSort
+{
+    public static void SelectionSortAlgorithm(IList<int> arr)
+    {
+        var n = arr.Count;
+
+        for (var i = 0; i < n - 1; i++)
+        {
+            // Find the minimum element in the remaining unsorted array
+            var minIndex = i;
+
+            for (var j = i + 1; j < n; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the element at index i
+            (arr[i], arr[minIndex]) = (arr[minIndex], arr[i]);
+        }
+    }
+}
